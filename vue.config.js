72793c0b02366089387aa9ff08df5e4754675541
erlan artykbaev <erik.2405@mail.ru>
2020-08-27 00:39:00 +0600

    mode: hash

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.export = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/vue-form-task/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/vue-form-task/' : '/',
+	mode: 'hash'
 }
